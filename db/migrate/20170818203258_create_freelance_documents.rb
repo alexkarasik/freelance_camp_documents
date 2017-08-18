c2cf94399283c9f603723bfc7cf4e0cf498a8d30
kinkorea <alexkarasik89@gmail.com>
2017-08-18 17:25:23 -0400

    set up rails database. generate some sample data, change the cors.rb in congig a little bit to configure it.

--- db/migrate/20170818203258_create_freelance_documents.rb
+++ db/migrate/20170818203258_create_freelance_documents.rb
@@ -1,9 +1,9 @@
 class CreateFreelanceDocuments < ActiveRecord::Migration[5.1]
   def change
     create_table :freelance_documents do |t|
-      t.string, :title
-      t.string, :description
-      t.text, :file_url
+      t.string :title
+      t.string :description
+      t.text :file_url
       t.text :image_url
 
       t.timestamps
